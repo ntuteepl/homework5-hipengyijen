b9ba5fe1a7fd6b1cc650c1b03baa4476d8c02955
彭 <146065452+hipengyijen@users.noreply.github.com>
2023-11-05 18:51:13 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<assert.h>
+
+int main(void) 
+{
+  int n;
+  int j;
+  double i;
+  scanf("%d %d",&j,&n);
+  if(j<=60)
+  {
+    i=j*n;
+  }
+  if(j>60&&j<=120)
+  {
+    i=n*60+1.33*n*(j-60);
+  }
+  if(j>120)
+  {
+    i=n*60+1.33*n*60+1.66*n*(j-120);
+  }
+  printf("%.1f",i);
+}
 
